8cde71c9f74d4967681eb79ba488ebcca54972e8
yu mingzhe <tylov.ohno@gmail.com>
2026-08-10 12:46:46 +0900

    clean

--- tests/support/performance.py
+++
@@ -1,5 +0,0 @@
-"""Test-facing import for the pre-registered Plan 03 statistics."""
-
-from fs_diloco.tools.paired_performance import PairedPerformanceResult, paired_noninferiority
-
-__all__ = ["PairedPerformanceResult", "paired_noninferiority"]
